4fd2759c73673df967b69ae69e9dda6636c7d65b
imanmafi <imafi@me.com>
2015-09-20 22:57:14 -0700

    add new component new-rental

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -10,4 +10,9 @@ export default Ember.Component.extend({
       this.set('isImageShowing', false)
     }
   }
+  delete(rental) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
+    }
 });
